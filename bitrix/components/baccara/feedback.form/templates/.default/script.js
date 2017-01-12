701096329b4e3afd1ab271cc1fa846dc73af4e0e
artshar <sharapudinov@mial.ru>
2017-01-12 03:29:49 +0300

    feedback_form update

--- bitrix/components/baccara/feedback.form/templates/.default/script.js
+++ bitrix/components/baccara/feedback.form/templates/.default/script.js
@@ -1,71 +1,84 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var phone_number =  $('.feedback_phone-inp');
-    var email =  $('.feedback_email-inp');
-    var fio =  $('.feedback_fio-inp');
-    var city =  $('.feedback_city-inp');
+    var phone_number = $('.feedback_phone-inp');
+    var email = $('.feedback_email-inp');
+    var fio = $('.feedback_fio-inp');
+    var city = $('.feedback_city-inp');
     var req_form = $('.feedback_form');
 
-    $('.feedback_select').on('change',function () {
+    $('.feedback_select').on('change', function () {
         debugger;
         $(this).prev().val($(this).val())
     })
-       
+
     phone_number.mask('+7(999) 999-99-99');
-    
-    req_form.submit(function(){
+
+    req_form.submit(function (e) {
         var result = true;
         $('.error').remove();
-        if(phone_number.val() == '' || phone_number.val() == null || phone_number.val() == undefined) {
+        if (!phone_number.val()) {
             result = false;
             req_form.prepend('<div class="error">Необходимо указать номер телефона</div>');
         }
-        else if(fio.val() == '' || fio.val() == null || fio.val() == undefined || city.val() == '' || city.val() == null || city.val() == undefined || city.val().indexOf("@") != -1 || fio.val().length < 5 || city.val().length < 3) {
-            if(fio.val() == '' || fio.val() == null || fio.val() == undefined) {
+        else if (!fio.val() || !city.val() || city.val().indexOf("@") != -1 || fio.val().length < 5 || city.val().length < 3) {
+            if (!fio.val()) {
                 req_form.prepend('<div class="error">Необходимо указать Ваши ФИО</div>');
             }
-            else if(fio.val().length < 5) {
+            else if (fio.val().length < 5) {
                 req_form.prepend('<div class="error">Поле ФИО должно содержать не менее 5 символов</div>');
             }
-            if(city.val() == '' || city.val() == null || city.val() == undefined || city.val().indexOf("@") != -1) {
+            if (!city.val() || city.val().indexOf("@") != -1) {
                 req_form.prepend('<div class="error">Необходимо указать Ваш город</div>');
             }
-            else if(city.val().length < 3) {
+            else if (city.val().length < 3) {
                 req_form.prepend('<div class="error">Поле Город должно содержать не менее 3 символов</div>');
             }
-            
+
             result = false;
         }
-        else if(email.val() == '' || email.val() == null || email.val() == undefined) {            
+        else if (email.val() == '' || email.val() == null || email.val() == undefined) {
             req_form.prepend('<div class="error">Необходимо заполнить поле Email</div>');
-            email.css({'border':'1px solid red'});            
-            result = false;        
-        } 
-        else if(!validateEmail(email.val())) {            
-            req_form.prepend('<div class="error">Некорректно заполнено поле Email</div>');
-            email.css({'border':'1px solid red'});            
-            result = false;        
-        } 
-        else {
-            var test_data = $('.feedback_form').serialize();
-            $.post('/ajax/request.php', {'data': test_data}).done(function(data) {
-                if(data) {  
-                    $('.request_popup').fadeIn('10').delay('1900').fadeOut('100');
-                    $('input:text').val('');
-                    $('textarea').val('');
-                }
-                else
-                    alert('Необходимо корректно ввести номер телефона. Попробуйте снова.');
-            });
+            email.css({'border': '1px solid red'});
+            result = false;
         }
-              
-        return false;
+        else if (!validateEmail(email.val())) {
+            req_form.prepend('<div class="error">Некорректно заполнено поле Email</div>');
+            email.css({'border': '1px solid red'});
+            result = false;
+        }
+        else {
+            var feedback_select = $('.feedback_select');
+            var ready = true;
+            feedback_select.each(function () {
+                if ( !$(this).prev().val()) ready = false;
+            });
+
+            if (!ready) {
+                e.preventDefault();
+                req_form.prepend('<div class="error">Не все данные заполнены</div>');
+            } else {
+                var test_data = $('.feedback_form').serialize();
+                $.post('/ajax/request.php', {'data': test_data}).done(function (data) {
+                    if (data) {
+                        $('.request_popup').fadeIn('10').delay('1900').fadeOut('100');
+                        $('input:text').val('');
+                        $('textarea').val('');
+                    }
+                    else
+                        alert('Что то пошло не так');
+                });
+            }
+
+            return false;
+        }
+
     });
-    
-    
+
+
     function validateEmail(email) {
         var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
         return re.test(email);
     }
-    
-});
+
+})
+;
